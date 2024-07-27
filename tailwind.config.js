98463ddcbac8bc6cec6fdeb0b476d603e1bab95a
VladProtchenko <andrii.panasenko295@gmail.com>
2024-07-27 17:12:38 +0300

    contact us section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,13 @@ module.exports = {
     extend: {
       colors: {
         customGray: "#3a3a3a",
-        bodyBg: '#f7f7f7',
+        bodyBg: "#f7f7f7",
+        contactBg: "#060a56",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
       },
     },
   },
   plugins: [],
 };
-
